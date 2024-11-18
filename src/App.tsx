65e7ffa59e62d542b7497cbf238ef3cc2b7f0d11
Admin <duchandsome0201@gmail.com>
2024-11-18 10:22:40 +0700

    delete some thing

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,27 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import enTranslations from "@shopify/polaris/locales/en.json";
-import CustomSkeletonPage from "./components/Skeleton/skeleton-page";
 import { useDocument } from "./hook/useDocument";
-import { AppProvider } from "@shopify/polaris";
-
-const HomePage = React.lazy(
-  () => import("./views/home")
-);
-const HoursPage = React.lazy(
-  () => import("./views/hours")
-);
-const DaysPage = React.lazy(
-  () => import("./views/days")
-);
+import "./App.css";
+const HomePage = React.lazy(() => import("./views/home"));
+const HoursPage = React.lazy(() => import("./views/hours"));
+const DaysPage = React.lazy(() => import("./views/days"));
 
 function App() {
   useDocument("Weather Web");
   return (
-    <AppProvider i18n={enTranslations}>
-      <Router>
-        <div>
-          {/* <HeaderBar /> */}
-          <Suspense fallback={<CustomSkeletonPage />}>
-            <Routes>
-              <Route
-                path="/"
-                element={<Navigate to="/home" />}
-              />
-              <Route
-                path="/home"
-                element={<HomePage />}
-              />
-              <Route
-                path="/hours"
-                element={<HoursPage />}
-              />
-              <Route
-                path="/days"
-                element={<DaysPage />}
-              />
+    <Router>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/hours" element={<HoursPage />} />
+          <Route path="/days" element={<DaysPage />} />
 
-              <Route
-                path="*"
-                element={<Navigate to="/home" />}
-              />
-            </Routes>
-          </Suspense>
-        </div>
-      </Router>
-    </AppProvider>
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import businessSlice from "./business/businessSlice";
-
-export default configureStore({
-  reducer: {
-    business: businessSlice,
-  },
-});
